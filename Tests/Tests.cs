e872e421ae7a56d3a5ea9823aaef2b2589e8fb1c
Michal Motyčka <michal.motycka@notino.com>
2021-11-25 17:50:37 +0100

    added empty app

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -7,9 +7,6 @@ namespace Tests
         [Test]
         public void Test1()
         {
-            //TODO tests
-            //For mocking use https://fakeiteasy.github.io/
-            //For assertions use https://fluentassertions.com/
         }
     }
 }
